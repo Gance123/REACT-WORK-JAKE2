6728b5455069a607b71941638572f02fb92ec550
Gance123 <110610675+Gance123@users.noreply.github.com>
2022-09-22 01:25:11 +0900

    proceed

---
+++ src/router/Router.tsx
@@ -0,0 +1,42 @@
+import { FC, memo } from "react";
+import { Route, Switch } from "react-router-dom";
+
+import { Login } from "../components/pages/Login";
+import { Page404 } from "../components/pages/Page404";
+import { homeRoutes } from "../router/HomeRoutes";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
+
+export const Router: FC = memo(() => {
+  return (
+    <Switch>
+      {/* ログインページルーター */}
+      <Route exact path="/">
+        <Login />
+      </Route>
+      {/* ホームページルーター */}
+      <Route
+        path="/home"
+        render={({ match: { url } }) => (
+          <Switch>
+            {homeRoutes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                <HeaderLayout>{route.children}</HeaderLayout>
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+      <Route path="*">
+        <Page404 />
+      </Route>
+    </Switch>
+  );
+});
+
+// <Route key={route.paht}  exact={route.exact} path={`${url}${route.path}`} >
+// {route.children}
+// </Route>
